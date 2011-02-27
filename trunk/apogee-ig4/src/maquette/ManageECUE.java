e6b15371ffb90b28514c32c7f1c0f7b5a02e0f67
pierre.jambet@gmail.com <pierre.jambet@gmail.com@e0ee766e-6ee4-06a9-eff5-331cd1b0dcda>
2011-02-27 22:00:41 +0000

    Projet netbeans apogee-ig4, contient les maquettes pour le moment

---
+++ trunk/apogee-ig4/src/maquette/ManageECUE.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package maquette;
+
+/**
+ *
+ * @author pierre
+ */
+public class ManageECUE extends javax.swing.JFrame {
+
+}
