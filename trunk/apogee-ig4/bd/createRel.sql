94b02030d8f84f08eeddede8c13d46c56530a0b0
pierre.jambet@gmail.com <pierre.jambet@gmail.com@e0ee766e-6ee4-06a9-eff5-331cd1b0dcda>
2011-03-01 09:49:07 +0000

    Ajout des scripts alter (ajout des contraintes) et drop(drop de l'ensemble des tables) et maj du fichier createRel.sql

--- trunk/apogee-ig4/bd/createRel.sql
+++ trunk/apogee-ig4/bd/createRel.sql
@@ -1,26 +1,24 @@
--- Creation de la base de données relationelles
+-- Creation de la base de données relationelles (20 tables)
 
 -- Creation de la base de données
 -- Nous avons choisi de créer une base de données OO afin d'utiliser
 -- cojointement les propriétés objet de la base et du langage java.
 
-select * from cat where table_name not like 'BIN%';
-/
+--select * from cat where table_name not like 'BIN%';
+--select constraint_name, table_name from user_constraints where constraint_name not like 'BIN%';
 
-select type_name from user_types;
+--select type_name from user_types;
 
 -- ECUE
 create table ECUE (
 	codeMatiere number,
-	libelleECUE varchar2(20),
+	libelleECUE varchar2(50),
 	nbheures number,
-	id_enseignant number,
+	idenseignant number,
 	codeue number,
-	constraint pkECUE primary key(codeMatiere),
-	constraint fkenseignant_ecue foreign key(id_enseignant) references enseignant(id_enseignant),
-	constraint fkue_ecue foreign key(codeue) references ue(codeue),
+	constraint pkECUE primary key(codeMatiere)
 );
-/
+
 
 -- UE
 create table UE (
@@ -28,13 +26,12 @@ create table UE (
 	nbECTS number,
 	libelleUE varchar2(50),
 	optionnel char(1),
-	id_enseignant number,
+	idenseignant number,
 	codesemestre number,
-	constraint pkUE primary key(codeUE),
-	constraint fkenseignant_ue foreign key(id_enseignant) references enseignant(id_enseignant),
-	constraint fksemestre_ue foreign key(codesemestre) references semestre(codesemestre)
+	constraint pkUE primary key(codeUE)
 );
-/
+
+
 
 -- Semestre
 
@@ -43,24 +40,21 @@ create table Semestre (
 	libellesemestre varchar2(10),
 	nbUEFacultatives number,
 	codeEtape number,
-	constraint pkSemestre primary key(codesemestre),
-	constraint fketape_ue foreign key(codeetape) references etape(codeetape)
+	constraint pkSemestre primary key(codesemestre)	
 );
-/
+
 
 -- Etape
 
 -- mettre les deux semestre en clé étrangeres ici ?
 create table Etape (
-	codeEtape number
+	codeEtape number,
 	versionEtape varchar2(10), -- le chiffre de l'année ?
-	id_enseignant number,
+	idenseignant number,
 	mnemo varchar2(10),
-	constraint pkEtape primary key(codeEtape),
-	constraint fkenseignant_etape foreign key(id_enseignant) references enseignant(id_enseignant),
-	constraint fkdepartement_etape foreign key(mnemo) references departement(mnemo)
+	constraint pkEtape primary key(codeEtape)	
 );
-/
+
 
 -- Departement
 
@@ -68,98 +62,95 @@ create table Departement (
 	versionDiplome varchar2(10),
 	nomDepartement varchar2(10),
 	mnemo varchar2(10),
-	id_enseignant number,
-	constraint pkDepartement primary key(mnemo),
-	constraint fkenseignant_departement foreign key(id_enseignant) references enseignant(id_enseignant)
+	idenseignant number,
+	constraint pkDepartement primary key(mnemo)
 );
-/
+
 
 create table Personne (
-	id_personne number,
+	idpersonne number,
 	nom varchar2(20),
 	prenom varchar2(20),
 	mail varchar2(20),
-	constraint pkPersonne primary key(id_personne)
+	constraint pkPersonne primary key(idpersonne)
 );
-/
+
 
 create table Utilisateur (
-	id_utilisateur number,
+	idutilisateur number,
 	mdp varchar2(10),
 	login varchar2(20),
-	constraint pkUtilisateur primary key(id_utilisateur)
+	nom varchar2(20),
+	prenom varchar2(20),
+	mail varchar2(20),
+	constraint pkUtilisateur primary key(idutilisateur)
 );
-/
+
 
 create table Etudiant (
+	numEtudiant number,
 	pointjuryannee number,
 	numINE varchar2(15),
-	numEtudiant number,
 	scoreTOEIC number,
-	id_personne number,
-	id_provenance number,
-	id_statut number,
-	id_nationalite number,
+	idpersonne number,
+	idprovenance number,
+	idstatut number,
+	idnationalite number,
 	codesemestre number,
-	codeUE number,
-	constraint pkEtudiant primary key(numEtudiant),
-	constraint fkpersonne_etud foreign key(id_personne) references personne(id_personne),
-	constraint fkprovenance_etud foreign key(id_provenance) references provenance(id_provenance),
-	constraint fkstatut_etud foreign key(id_statut) references statut(id_statut),
-	constraint fknationalite_etud foreign key(id_nationalite) references nationalite(id_nationalite),
-	constraint fksemestre_etud foreign key(codesemestre) references semestre(codesemestre)	
+	codeEtape number,
+	nom varchar2(20),
+	prenom varchar2(20),
+	mail varchar2(20),
+	constraint pkEtudiant primary key(numEtudiant)
 );
-/
+
 
 create table VAE (
 	codeue number,
-	numEtudiant number,
-	constraint fkue_vae foreign key(codeue) references ue(codeue),
-	constraint fketud_vae foreign key(numetudiant) references etudiant(numEtudiant)
+	numEtudiant number	
 );
-/
 
 create table choixUE (
 	codeue number,
-	numEtudiant number,
-	constraint fkue_choixUE foreign key(codeue) references ue(codeue),
-	constraint fketud_choixUE foreign key(numetudiant) references etudiant(numEtudiant)
+	numEtudiant number	
 );
-/
+
 
 create table APDJ (
 	codeue number,
-	numEtudiant number,
-	constraint fkue_apdj foreign key(codeue) references ue(codeue),
-	constraint fketud_apdj foreign key(numetudiant) references etudiant(numEtudiant)
+	numEtudiant number	
 );
-/
 
+	
+-- Qu'est ce que l'enseignant a en plus de la secretaire ?
 create table Enseignant (
-	id_enseignant number,
-	id_utilisateur number,
-	constraint pkEnseignant primary key(id_enseignant),
-	constraint fkutilisateur_ens foreign key(id_utilisateur) references utilisateur(id_utilisateur)
+	idenseignant number,
+	mdp varchar2(10),
+	login varchar2(20),
+	nom varchar2(20),
+	prenom varchar2(20),
+	mail varchar2(20),
+	constraint pkEnseignant primary key(idenseignant)
 );
-/
 
-create type TSecretaire (
-	id_secretaire number,
-	id_utilisateur number,
-	constraint pkSecretaire primary key(id_secretaire),
-	constraint fkutilisateur_sec foreign key(id_utilisateur) references utilisateur(id_utilisateur)
+
+create table Secretaire (
+	idsecretaire number,
+	mdp varchar2(10),
+	login varchar2(20),
+	nom varchar2(20),
+	prenom varchar2(20),
+	mail varchar2(20),
+	constraint pkSecretaire primary key(idsecretaire)
 );
-/
-
 
 -- Statuts
 
 create table Statut (
-	id_statut number,
+	idstatut number,
 	libelle varchar2(20),
-	constraint pkStatut primary key(id_statut)
+	constraint pkStatut primary key(idstatut)
 );
-/
 
 
 -- Notes
@@ -169,56 +160,47 @@ create table Note (
 	noteSession2 number,
 	numEtudiant number,
 	codeMatiere number,
-	constraint pkNote primary key(idnote),
-	constraint fketudiant_note foreign key(numEtudiant) references etudiant(numEtudiant),
-	constraint fkecue_note foreign key(codeMatiere) references ecue(codeMatiere)
+	constraint pkNote primary key(idnote)	
 );
-/
 
 
-create table PointJury (
-	idPointJury number,
+create table PointsJury (
+	idPointsJury number,
 	nbPoints number,
 	numEtudiant number,
 	codesemestre number,
-	constraint pkPointJury primary key(idPointJury),
-	constraint fketudiant_pj foreign key(numEtudiant) references etudiant(numEtudiant),
-	constraint fksemectre_pj foreign key(codeSemestre) references semestre(codeSemestre)
+	constraint pkPointJury primary key(idPointsJury)
 );
-/
 
-create Redoublant (
-	id_redoublant number,
+
+create table Redoublant (
+	idredoublant number,
 	moyenne number,
 	numEtudiant number,
 	codesemestre number,
-	constraint pkRedoublant primary key(id_redoublant),
-	constraint fketudiant_red foreign key(numEtudiant) references etudiant(numEtudiant),
-	constraint fksemestre_red foreign key(codeSemestre) references semestre(codeSemestre)
+	constraint pkRedoublant primary key(idredoublant)
 );
-/
 
+	
 create table Provenance (
-	id_provenance number,
+	idprovenance number,
 	libelleProvenance varchar2(20),
-	constraint pkProvenance primary key(id_number)
+	constraint pkProvenance primary key(idprovenance)
 );
-/
+
 
 create table etranger (
-	id_etranger number,
+	idetranger number,
 	moyenne number,
 	numEtudiant number,
 	codesemestre number,
-	constraint pkEtranger primary key(id_etranger),
-	constraint fketudiant_etr foreign key(numEtudiant) references etudiant(numEtudiant),
-	constraint fksemestre_etr foreign key(codeSemestre) references semestre(codeSemestre)
+	constraint pkEtranger primary key(idetranger)
 );
-/
+
 
 create table nationalite (
-	id_nationalite number,
+	idnationalite number,
 	libelleNationalite varchar2(20),
-	constraint pkNationalite primary key(id_nationalite)
+	constraint pkNationalite primary key(idnationalite)
 );
-/
+
---
+++ trunk/apogee-ig4/bd/dropRel.sql
@@ -0,0 +1,27 @@
+drop table vae;
+drop table choixUE;
+drop table apdj;
+drop table note;
+drop table pointsjury;
+drop table redoublant;
+drop table etranger;
+
+drop table etudiant;
+
+drop table provenance;
+drop table statut;
+drop table nationalite;
+
+drop table ecue;
+drop table ue;
+drop table semestre;
+drop table etape;
+drop table departement;
+
+drop table personne;
+drop table utilisateur;
+
+drop table enseignant;
+drop table secretaire;
+
+
