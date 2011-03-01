dfc8d693acac6a42d08bb7b6155a16c515432df6
benjamin.michel.cn <benjamin.michel.cn@e0ee766e-6ee4-06a9-eff5-331cd1b0dcda>
2011-03-01 17:37:42 +0000

    vo_terminé

--- apogee-ig4/bd/createView.sql
+++ apogee-ig4/bd/createView.sql
@@ -9,28 +9,37 @@ create or replace type TEtud as object (
 	noteSession1 number,
 	noteSession2 number
 );
+/
 
 create or replace type TEtud_nt as table of TEtud;
+/
 
 create or replace type TECUE as object (
-	codeMatiere number,
+	codeMatiere varchar2(10),
 	libelleECUE varchar2(50),
 	nbheures number,
 	idenseignant number,
-	codeue number,
+	codeue varchar2(10),
 	listeEtud TEtud_nt
 );
+/
 
 create or replace view VO_ECUE of TECUE
 with object identifier(codeMatiere)
 as
 select ec.codeMatiere, ec.libelleECUE, ec.nbheures, ec.idenseignant, ec.codeue, cast( multiset(
-	select e.numEtudiant, e.nom, e.prenom,
+	select distinct e.numEtudiant, e.nom, e.prenom,
 		n.noteSession1, n.noteSession2
 	from Etudiant e, Note n
-	where n.numEtudiant = e.numEtudiant
-	and e.numEtudiant in
-		get_liste_etud_ecue(ec.codeMatiere)
+	where n.numEtudiant(+)= e.numEtudiant
+	and e.numEtudiant in (
+		Select le.numEtudiant
+		from table(get_liste_etud_ecue(ec.codeMatiere)) le
+		)
 	)as TEtud_nt)
 from ECUE ec;
+/
+
+
+
 
