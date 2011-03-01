af29368b8edbc358a80e0a3f967bee56438fa040
benjamin.michel.cn <benjamin.michel.cn@e0ee766e-6ee4-06a9-eff5-331cd1b0dcda>
2011-03-01 12:55:24 +0000

    create view

--- trunk/apogee-ig4/bd/createView.sql
+++ trunk/apogee-ig4/bd/createView.sql
@@ -2,14 +2,32 @@
 
 -- Creation du type qui composera la vue;
 
-create type TEtud as object 
+create or replace type TEtud as object (
+	numEtudiant number,
+	nom varchar2(20),
+	prenom varchar2(20),
+	noteSession1 number,
+	noteSession2 number
+);
 
-create type TEtud_nt as table of TEtud;
+create or replace type TEtud_nt as table of TEtud;
 
-create type TECUE as object (
+create or replace type TECUE as object (
 	codeMatiere number,
 	libelleECUE varchar2(50),
 	nbheures number,
 	idenseignant number,
-	codeue number
-)
+	codeue number,
+	listeEtud TEtud_nt
+);
+
+create or replace view VO_ECUE of TECUE
+with object identifier(codeMatiere)
+as
+select ec.codeMatiere, ec.libelleECUE, ec.nbheures, ec.idenseignant, ec.codeue, cast( multiset(
+	select e.numEtudiant, e.nom, e.prenom,
+		n.noteSession1, n.noteSession2
+	from Etudiant e, Note n
+	where e.numEtudiant in get_
+	)as TEtud_nt)
+from ECUE ec;
