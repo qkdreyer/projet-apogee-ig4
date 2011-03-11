d50fb924e368fc2af99e549d5f26155c638fd41a
benjamin.michel.cn@gmail.com <benjamin.michel.cn@gmail.com@e0ee766e-6ee4-06a9-eff5-331cd1b0dcda>
2011-03-11 14:43:04 +0000

    correction pour utilisation du pattern singleton

--- apogee-ig4/src/fr/GCAM/StudentManager/UI/AbstractUIFactory.java
+++ apogee-ig4/src/fr/GCAM/StudentManager/UI/AbstractUIFactory.java
@@ -16,21 +16,29 @@ public abstract class AbstractUIFactory {
     protected static AbstractUIFactory fact = null;
 
     public abstract UI getUIUtilisateur(String s);
+
     public abstract UI getUIEtudiant(String s);
+
     public abstract UI getUIECUE(String s);
+
     public abstract UI getUIUE(String s);
+
     public abstract UI getUIEtape(String s);
+
     public abstract UI getUIDepartement(String s);
+
     public abstract UI getUISemestre(String s);
 
     public static AbstractUIFactory getUIFactory(String s) {
-	if (s.equals("c")) {
-	    fact = new ConsoleFactory();
-	} else if (s.equals("g")) {
-	    fact = new GUIFactory();
-	} else {
-	    System.err.println("No UI declared");
-	}
-	return fact;
+        if (fact == null) {
+            if (s.equals("c")) {
+                fact = new ConsoleFactory();
+            } else if (s.equals("g")) {
+                fact = new GUIFactory();
+            } else {
+                System.err.println("No UI declared");
+            }
+        }
+        return fact;
     }
 }
