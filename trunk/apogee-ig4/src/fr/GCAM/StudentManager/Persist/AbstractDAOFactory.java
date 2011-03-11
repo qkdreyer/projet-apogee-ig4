24428e01b20d753162e9fbba31de0867087d18a4
benjamin.michel.cn@gmail.com <benjamin.michel.cn@gmail.com@e0ee766e-6ee4-06a9-eff5-331cd1b0dcda>
2011-03-11 14:43:04 +0000

    correction pour utilisation du pattern singleton

--- trunk/apogee-ig4/src/fr/GCAM/StudentManager/Persist/AbstractDAOFactory.java
+++ trunk/apogee-ig4/src/fr/GCAM/StudentManager/Persist/AbstractDAOFactory.java
@@ -4,7 +4,6 @@ package fr.GCAM.StudentManager.Persist;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import fr.GCAM.StudentManager.Persist.DB.DBFactory;
 import fr.GCAM.StudentManager.Persist.XML.XMLFactory;
 import java.util.Observable;
@@ -18,21 +17,29 @@ public abstract class AbstractDAOFactory extends Observable {
     protected static AbstractDAOFactory fact = null;
 
     public abstract DAO getDAOUtilisateur();
+
     public abstract DAO getDAOEtudiant();
+
     public abstract DAO getDAOECUE();
+
     public abstract DAO getDAOUE(String s);
+
     public abstract DAO getDAOSemestre(String s);
+
     public abstract DAO getDAOEtape(String s);
+
     public abstract DAO getDAODepartement(String s);
 
     public static AbstractDAOFactory getDAOFactory(String s) {
-	if (s.equals("db")) {
-	    fact = new DBFactory();
-	} else if (s.equals("xml")) {
-	    fact = new XMLFactory();
-	} else {
-	    System.err.println("No factory declared");
-	}
+        if (fact == null) {
+            if (s.equals("db")) {
+                fact = new DBFactory();
+            } else if (s.equals("xml")) {
+                fact = new XMLFactory();
+            } else {
+                System.err.println("No factory declared");
+            }
+        }
         return fact;
     }
 }
