0ea649954879ea847fc28e711f6606e6072c74d6
quentin.dreyer <quentin.dreyer@e0ee766e-6ee4-06a9-eff5-331cd1b0dcda>
2011-03-12 19:10:52 +0000

    Controller/Console update

--- apogee-ig4/src/fr/GCAM/StudentManager/Controller/ControllerEtape.java
+++ apogee-ig4/src/fr/GCAM/StudentManager/Controller/ControllerEtape.java
@@ -5,10 +5,56 @@
 
 package fr.GCAM.StudentManager.Controller;
 
+import fr.GCAM.StudentManager.POJO.Etape;
+import fr.GCAM.StudentManager.Persist.AbstractDAOFactory;
+import fr.GCAM.StudentManager.Persist.DAO;
+import fr.GCAM.StudentManager.UI.UI;
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  *
  * @author Quentin
  */
-public class ControllerEtape {
+public class ControllerEtape implements Observer {
+
+    private UI disp;
+    private DAO<Etape> etapeDAO;
+    private Etape etape;
+
+    public ControllerEtape(UI disp, String s) {
+        this.disp = disp;
+        etapeDAO = AbstractDAOFactory.getDAOFactory(s).getDAOEtape();
+        etape = new Etape();
+    }
+
+    public void update(Observable o, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void handleMessage(String message) throws Exception {
+        String[] msg = message.split(" ");
+
+        if (msg[0].equals("#find") && msg.length == 2) { // #find pstia602
+            this.find(msg);
+        } else if (msg[0].equals("#update")) {
+            this.update(msg);
+        } else if (msg[0].equals("#quit")) {
+            this.quit();
+        }
+    }
+
+    public void find(String[] msg) throws Exception {
+        etape = (Etape) etapeDAO.find(msg[1]);
+        disp.display(etape.toString());
+    }
+
+    public void update(String[] msg) throws Exception {
+        etapeDAO.update(etape);
+    }
+
+    public void quit() {
+        System.exit(0);
+    }
 
 }
--- apogee-ig4/src/fr/GCAM/StudentManager/UI/Console/ConsoleECUE.java
+++ apogee-ig4/src/fr/GCAM/StudentManager/UI/Console/ConsoleECUE.java
@@ -6,8 +6,6 @@ package fr.GCAM.StudentManager.UI.Console;
 
 import fr.GCAM.StudentManager.Controller.ControllerECUE;
 import fr.GCAM.StudentManager.POJO.ECUE;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 /**
  *
@@ -19,24 +17,6 @@ public class ConsoleECUE extends Console<ECUE> {
 
     public ConsoleECUE(String s) {
         contr = new ControllerECUE(this, s);
-	this.accept();
-    }
-
-     /**
-     * This method waits for input from the console. Once it is received, it sends it to the client's message handler.
-     */
-    public void accept() {
-        try {
-            BufferedReader fromConsole = new BufferedReader(new InputStreamReader(System.in));
-            String message;
-
-            while (true) {
-                message = fromConsole.readLine();
-                contr.handleMessage(message);
-            }
-        } catch (Exception ex) {
-            System.out.println("Unexpected error while reading from console!");
-        }
     }
 
     public void display(String msg) {
