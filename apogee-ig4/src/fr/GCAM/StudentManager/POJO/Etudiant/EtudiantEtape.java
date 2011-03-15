61f83ad676c98969df34cd3ae16bd76b3b12e071
quentin.dreyer <quentin.dreyer@e0ee766e-6ee4-06a9-eff5-331cd1b0dcda>
2011-03-15 17:11:33 +0000

    new Etudiant

---
+++ apogee-ig4/src/fr/GCAM/StudentManager/POJO/Etudiant/EtudiantEtape.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.GCAM.StudentManager.POJO.Etudiant;
+
+/**
+ *
+ * @author Quentin
+ */
+public class EtudiantEtape {
+
+    private int scoreToeic;
+    private float pointJuryAnnee;
+
+    public float getPointJuryAnnee() {
+	return pointJuryAnnee;
+    }
+
+    public void setPointJuryAnnee(float pointJuryAnnee) {
+	this.pointJuryAnnee = pointJuryAnnee;
+    }
+
+    public int getScoreToeic() {
+	return scoreToeic;
+    }
+
+    public void setScoreToeic(int scoreToeic) {
+	this.scoreToeic = scoreToeic;
+    }
+
+    @Override
+    public String toString() {
+	return super.toString() + " - "
+		+ "Score TOEIC : " + getScoreToeic()
+		+ "Point Jury Annee : " + getPointJuryAnnee();
+    }
+}
