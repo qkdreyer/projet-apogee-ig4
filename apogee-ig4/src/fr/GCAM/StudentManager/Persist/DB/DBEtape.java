b27e5e95b2a0f065c6f7ae6ae33f93e79ccc4e56
benjamin.michel.cn@gmail.com <benjamin.michel.cn@gmail.com@e0ee766e-6ee4-06a9-eff5-331cd1b0dcda>
2011-03-17 08:20:54 +0000

    find du pojo Etape (DB) terminé. Liste EtudiantEtape, Liste EtudiantSemestre et Liste UE terminé. MAJ de EtudiantSemestre pour stocker moyenne (etranger, redoublant). Tests nécessaires.

--- apogee-ig4/src/fr/GCAM/StudentManager/Persist/DB/DBEtape.java
+++ apogee-ig4/src/fr/GCAM/StudentManager/Persist/DB/DBEtape.java
@@ -6,6 +6,7 @@ package fr.GCAM.StudentManager.Persist.DB;
 
 import fr.GCAM.StudentManager.POJO.Etape;
 import fr.GCAM.StudentManager.POJO.Etudiant.EtudiantEtape;
+import fr.GCAM.StudentManager.POJO.Etudiant.EtudiantSemestre;
 import fr.GCAM.StudentManager.Persist.DB.Etudiant.DBEtudiantEtape;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -66,7 +67,7 @@ public class DBEtape extends DB<Etape> {
 
         Statement s = this.conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         //recherche le contenu de l'étape
-        ResultSet result = s.executeQuery("SELECT * from VO_Etape where codeEtape = '"
+        ResultSet result = s.executeQuery("SELECT * from VO_Etape WHERE codeEtape = '"
                 + (String) id
                 + "'");
         //enregistrement des données récupérées
@@ -83,7 +84,8 @@ public class DBEtape extends DB<Etape> {
             ResultSet resultEtudiant = s.executeQuery("SELECT numEtudiant FROM Etudiant"
                     + "WHERE codeEtape = '" + (String)id + "'");
 
-            //Création des objets EtudiantEtape dans la listeEtud
+            //Création des objets EtudiantEtape dans la listeEtud et insertion
+            //des informations basiques
             if (resultEtudiant.first()){
                 do {
                     etape.getListeEtud().add(new DBEtudiantEtape(conn).find(result.getString("codeEtudiant")));
@@ -103,19 +105,65 @@ public class DBEtape extends DB<Etape> {
                 semestre.setCodeSemestre(result.getString("codeSemestre"));
                 semestre.setNbUEFacultatives(result.getInt("nbUEFacultatives"));
 
-                //TODO: Enregistrement dans listeEtudiant des EtudiantsSemestre
                 
                 //recopie des informations de chaque étudiant dans la liste des
                 //étudiants du semestre
                 for (EtudiantEtape e : etape.getListeEtud()){
-                    ResultSet resultEtudSemPJ = s.executeQuery("SELECT null FROM PointsJury"
-                        + "WHERE codeSemestre = '" + semestre.getCodeSemestre() + "'"
-                        + "AND numEtudiant = " + e.getNumEtudiant());
 
+                    //Créer un EtudiantSemestre avec les infos de l'étudiant Etape
+                    EtudiantSemestre etudSem = new EtudiantSemestre(
+                            e.getNumEtudiant(),
+                            e.getNumIne(),
+                            e.getLibelleProvenance(),
+                            e.getLibelleStatut(),
+                            e.getLibelleNationalite(),
+                            e.getNom(),
+                            e.getPrenom(),
+                            e.getMail(),
+                            0, //Nombre de points jury par défaut
+                            false, //Etranger par défaut
+                            false); //Redoublant par défaut
+
+                    //si l'étudiant a reçu des points jury
+                    ResultSet resultEtudSemPJ = s.executeQuery("SELECT nbpoints FROM PointsJury"
+                        + " WHERE codeSemestre = '" + semestre.getCodeSemestre() + "'"
+                        + " AND numEtudiant = " + e.getNumEtudiant());
+                    
+                    //si l'étudiant passe son semestre à l'étranger
+                    ResultSet resultEtudEtranger = s.executeQuery("SELECT moyenne FROM Etranger"
+                            + " WHERE codeSemestre = '" + semestre.getCodeSemestre() +"'"
+                            + " AND numEtudiant = " +e.getNumEtudiant());
+
+                    //si l'étudiant a déjà une note en raison d'un redoublement
+                    ResultSet resultEtudRedouble = s.executeQuery("SELECT moyenne FROM Redoublant"
+                            + " WHERE codeSemestre = '" + semestre.getCodeSemestre() +"'"
+                            + " AND numEtudiant = " +e.getNumEtudiant());
+                    
                     //Ajout des information d'un étudiant
 
+                    //Si l'étudiant a des points jury:
+                    if (resultEtudSemPJ.first()){
+                        etudSem.setPointJurySemestre(resultEtudSemPJ.getInt("nbpoints"));
+                    }
+
+                    //si l'étudiant passe un semestre à l'étranger
+                    //on indique qu'il est à l'étranger
+                    //on donne la moyenne qu'il a obtenut
+                    if (resultEtudEtranger.first()){
+                        etudSem.setEtranger(true);
+                        etudSem.setMoyEtranger(resultEtudEtranger.getInt("moyenne"));
+                    }
+
+                    //Si l'étudiant est un redoublant
+                    //on indique qu'il est redoublant
+                    //on donne la moyenne qu'il a obtenut
+                    if (resultEtudRedouble.first()){
+                        etudSem.setRedoublant(true);
+                        etudSem.setMoyRedoublant( resultEtudRedouble.getInt("moyenne"));
+                    }
+
                     //Ajout de l'étudiant dans listeEtudiant du semestre
-                    //semestre.getListeEtud().add();
+                    semestre.getListeEtud().add(etudSem);
                 }
 
                 
