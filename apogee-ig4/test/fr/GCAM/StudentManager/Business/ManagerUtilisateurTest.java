8207ad48b57ce0c9a146d9b3fb80f852b78e1523
benjamin.michel.cn@gmail.com <benjamin.michel.cn@gmail.com@e0ee766e-6ee4-06a9-eff5-331cd1b0dcda>
2011-03-18 12:57:36 +0000

    test manage utilisateur

---
+++ apogee-ig4/test/fr/GCAM/StudentManager/Business/ManagerUtilisateurTest.java
@@ -0,0 +1,92 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.GCAM.StudentManager.Business;
+
+import java.util.ArrayList;
+import java.sql.Connection;
+import fr.GCAM.StudentManager.Persist.DB.ConnectionDB;
+import fr.GCAM.StudentManager.POJO.Utilisateur;
+import java.util.HashMap;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ben
+ */
+public class ManagerUtilisateurTest {
+
+    private static Connection conn;
+    private static Utilisateur util_m;
+
+    public ManagerUtilisateurTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+	conn = ConnectionDB.getConnection();
+	ArrayList<Utilisateur.Responsabilite> listResp = new ArrayList<Utilisateur.Responsabilite>();
+	listResp.add(new Utilisateur.Responsabilite("testVDip", "Departement"));
+	listResp.add(new Utilisateur.Responsabilite("testCEt", "Etape"));
+	listResp.add(new Utilisateur.Responsabilite("testCUE", "UE"));
+	listResp.add(new Utilisateur.Responsabilite("TEST001", "ECUE"));
+
+	util_m = new Utilisateur(9999, "prenomdansbase", "nomdansbase", "motdepasse", "testMail", listResp);
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of login method, of class ManagerUtilisateur.
+     */
+    @Test
+    public void testLogin() throws Exception {
+        System.out.println("login");
+        ManagerUtilisateur instance = null;
+        instance.login();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of login method, of class ManagerUtilisateur.
+     */
+    @Test
+    public void testLogin_3args() {
+
+        //base de données
+        System.out.println("login");
+        String nom = "nomdansbase";
+        String prenom = "prenomdansbase";
+        String mdp = "motdepasse";
+        ManagerUtilisateur instance = new ManagerUtilisateur("db");
+        HashMap expResult = new HashMap();
+        expResult.put("nom", "nomdansbase");
+        expResult.put("prenom", "prenomdansbase");
+        expResult.put("mds", "motdepasse");
+
+        HashMap result = instance.login(nom, prenom, mdp);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
